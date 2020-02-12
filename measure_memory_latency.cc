d9bed85417717eccfdbd9e01b332113a2f9bb6b8
curtis-m-l <54925341+curtis-m-l@users.noreply.github.com>
2020-02-12 12:05:53 -0800

    Initial Commit

---
+++ measure_memory_latency.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <chrono>
+
+/*
+Created by Maxx Curtis
+for CSCI 389 Homework 1
+
+
+*/
+
+// Constants
+int BUFFER_POWER_MIN = 10
+int BUFFER_POWER_MAX = 26
+
+int main() {
+	// Records latency of reading one byte from buffers of various sizes
+
+	// for each possible size:
+	for (int i = BUFFER_POWER_MIN; i < BUFFER_POWER_SIZE; i++) {
+		char buffer[i];
+
+	}
+
+		// for a certain number of times:
+			// grab a number and measure the time
+
+std::cout << "Hello!\n";
+
+
+	return 0;
+}
